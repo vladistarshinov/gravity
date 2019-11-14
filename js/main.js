412f407091f0ef99156cd0f9d222405087b09c33
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2019-11-14 22:33:25 +0300

    Изменения в 4 блоке, добавили скрипт для табов

--- js/main.js
+++ js/main.js
@@ -18,4 +18,17 @@ $(function() {
       }
     }
   });
+
+  $("ul.tabs__caption").on("click", "li:not(.active)", function(e) {
+    e.preventDefault();
+    $(this)
+      .addClass("active")
+      .siblings()
+      .removeClass("active")
+      .closest("section.tabs")
+      .find("div.tabs__content")
+      .removeClass("active")
+      .eq($(this).index())
+      .addClass("active");
+  });
 });
