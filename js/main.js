9abe3d4df67200bee98442f04bab7e77b7297c3d
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2019-11-14 23:47:35 +0300

    Добавили блок 6 с отзывами

--- js/main.js
+++ js/main.js
@@ -1,11 +1,31 @@
 $(function() {
-  $(".owl-carousel").owlCarousel({
+  $(".works-carousel").owlCarousel({
     loop: true,
     margin: 30,
     nav: true,
     dots: false,
     navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
     startPosition: 1,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+
+  $(".contributor-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    navText: ['<img src="img/left.svg">', '<img src="img/right.svg">'],
+    //startPosition: 1,
     responsive: {
       0: {
         items: 1
